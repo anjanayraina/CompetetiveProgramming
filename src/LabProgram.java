4947ecc0f1be1d2b3f2dc4b3e811e65e3a59a80f
Anjanay Raina <anjanayraina326@gmail.com>
2022-10-02 14:17:21 +0530

    File input taken

--- src/LabProgram.java
+++ src/LabProgram.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class LabProgram {
@@ -7,8 +9,15 @@ public class LabProgram {
     public static void main(String args[]) throws FileNotFoundException {
 
 
-        File file = new File("");
+        File file = new File("C:\\Users\\anjan\\IdeaProjects\\CompetetiveProgramming\\src\\food.txt");
+        // add the path of the food file that you saved in your system in the file class above
         Scanner snc = new Scanner(file);
+        HashMap<String  , ArrayList<String >> map  = new HashMap<>();
+        while(snc.hasNext()){
 
+            String temp  = snc.nextLine();
+
+
+        }
     }
 }
