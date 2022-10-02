4980df276c3b3c551b0d2f8426a4a7cf7142832e
Anjanay Raina <anjanayraina326@gmail.com>
2022-10-02 14:05:18 +0530

    File input base created

---
+++ src/LabProgram.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LabProgram {
+
+    public static void main(String args[]) throws FileNotFoundException {
+
+
+        File file = new File("");
+        Scanner snc = new Scanner(file);
+
+    }
+}
